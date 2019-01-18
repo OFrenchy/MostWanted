83f36184159c81adaf594d093ddb7b942f0ca371
Dean Bourgeois <deanbourgeois@OFrenchy.local>
2019-01-18 16:49:37 -0600

    added & implemented isAlphabetic, added isValidNumber function for later use

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Build all of your functions for displaying and gathering information below (GUI)
 //      with the program (display values for the various traits of the found person).
 // - As a user, I want to be able look up someone’s descendants after I find them with 
 //      the program (display the names of the descendants), using recursion.
-// - As a user, I want to be able look up someone’s immediate family members after I 
+// -X As a user, I want to be able look up someone’s immediate family members after I 
 //      find them with the program (display the names of the family members and their 
 //      relation to the found person).
 // - As a developer, I want to use the array.map() advanced array method within my project.
@@ -46,46 +46,47 @@ Build all of your functions for displaying and gathering information below (GUI)
 // app is the function called to start the entire application
 function app(people){
     
-    // people = people.filter(function(el) {
-    //     el.fullName = el.firstName + " " + el.lastName;
-    //     el.age = calculate_age(el.dob);
-    //     // if(el.firstName === "Joy" && el.lastName === "Madden") {
-    //     //     alert("found Joy Madden");
-    //     //     alert(el.fullName);
-    //         return true;
-    //     // }
 
-    // });
+    // Add these fields to all people at the start
     people = people.map(function (el) {
         el.fullName = el.firstName + " " + el.lastName;
         el.age = calculate_age(el.dob);
+        el.relationship = "";
         return el;
     });
+    // console.log(people);
+
+    //alert("first one is: " + (!isNaN("111") && "111" != "" && ("111").indexOf(" ") == -1));
+    
+    // test for numeric inputs
+    alert("test first one is: " + ((!isNaN("111"))  &&  ("111" != "") && ("111").indexOf(" ") == -1));
+    alert("test  2nd  one is: " + ((!isNaN("1 11"))  &&  ("11 1" != "") && ("1 11").indexOf(" ") == -1));
+
+    // test for alphabetic characters
+    alert("testing isAlphabetic function: " + isAlphabetic("abc") );
+    alert("testing isAlphabetic function: " + isAlphabetic("ab12cd"));
 
 
+    
+    var searchResults;
+    var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'y' for yes or 'n' for no", yesNo).toLowerCase();
+    switch(searchType){
+        case 'y':
+            // this next line works
+            // let thisPerson = searchByName(people);
+            let searchResults = searchForPeople("name", people) ;
+            mainMenu(searchResults[0], people);
+            break;
+        case 'n':
+            // TODO: search by traits
+            searchResults = searchForPeople("traits", people);
 
- console.log(people);
-
-
-  var searchResults;
-  var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'y' for yes or 'n' for no", yesNo).toLowerCase();
-  switch(searchType){
-    case 'y':
-      // this next line works
-      // let thisPerson = searchByName(people);
-      let searchResults = searchForPeople("name", people) ;
-      mainMenu(searchResults[0], people);
-      break;
-    case 'n':
-      // TODO: search by traits
-      searchResults = searchForPeople("traits", people);
-
-      break;
-    default:
-      alert("Invalid input. Please try again!");
-      app(people); // restart app
-    break;
-  }
+            break;
+        default:
+            alert("Invalid input. Please try again!");
+            app(people); // restart app
+        break;
+    }
 }
 
 // Menu function to call once you find who you are looking for
@@ -95,33 +96,33 @@ function mainMenu(person, people){
         as well as the entire original dataset of people. We need people in 
         order to find descendants and other information that the user may want. */
 
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
+    if(!person){
+        alert("Could not find that individual.");
+        return app(people); // restart
+    }
 
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + 
-    " . Do you want to know their 'info', 'family', or 'descendants'? " + 
-    "Type the option you want or 'restart' or 'quit'");
+    var displayOption = prompt("Found " + person.firstName + " " + person.lastName + 
+        " . Do you want to know their 'info', 'family', or 'descendants'? " + 
+        "Type the option you want or 'restart' or 'quit'");
 
   switch(displayOption){
     case "info":
-      // DONE!  TODO: get person's info
-      displayPerson(person);
-      break;
+        // DONE!  TODO: get person's info
+        displayPerson(person);
+        break;
     case "family":
-      // TODO: get person's family
-      let searchResults = searchForPeople("family", people, person.id);
+        // TODO: get person's family
+        let searchResults = searchForPeople("family", people, person.id);
 
-      //displayPeople
-      if (searchResults.length >= 1 ){
-          displayPeople(searchResults);
-      }
-      else {
-        alert("No family found for " + person.fullName);
-      }
-
-      break;
+        //displayPeople
+        if (searchResults.length >= 1 ){
+            displayPeople(searchResults);
+        }
+        else {
+          // TODO NEED TO TEST THIS
+          alert("No family found for " + person.fullName);
+        }
+        break;
     case "descendants":
       // TODO: get person's descendants
       break;
@@ -141,8 +142,8 @@ function searchByName(people){
 
     let filteredPeople = people.filter(function(el) {
         if(el.firstName === firstName && el.lastName === lastName) {
-            el.fullName = el.firstName + " " + el.lastName;
-            el.age = calculate_age(el.dob);
+        //     el.fullName = el.firstName + " " + el.lastName;
+        //     el.age = calculate_age(el.dob);
          return el;
         }
     });
@@ -171,11 +172,16 @@ function searchForPeople(searchType = "", people, idToSearch = -1){
         filteredPeople = people.filter(function(el) {
             //if(el.currentSpouse === idToSearch || el.parents === idToSearch) {
             if(el.currentSpouse === idToSearch || el.parents.indexOf (idToSearch) >= 0) {
+                if(el.currentSpouse === idToSearch) {
+                  el.relationship = "Spouse";
+                }
+                else{
+                  el.relationship = "Child";
+                }
                 return el;
             }
         });
         //return filteredPeople;
-
     }
     else if (searchType === "descendants") {
         var searchCriteria = "fill this in";
@@ -194,9 +200,19 @@ function searchForPeople(searchType = "", people, idToSearch = -1){
 // alerts a list of people
 function displayPeople(people){
   alert(people.map(function(person){
-    return person.firstName + " " + person.lastName;
+    //return if(person.relationship != ""){return person.relationship + ": ";} + person.fullName;
+    let returnString = "";
+    if (person.relationship != "") {
+      returnString = person.relationship + ": ";
+    }
+    return returnString + person.fullName;
   }).join("\n"));
 }
+// function displayPeople(people){
+//   alert(people.map(function(person){
+//     return person.firstName + " " + person.lastName;
+//   }).join("\n"));
+//}
 
 function displayPerson(person){
   // print all of the information about a person:
@@ -227,9 +243,26 @@ function yesNo(input){
 
 // helper function to pass in as default promptFor validation
 function chars(input){
-  return true; // default validation only
-}
+  
+  return isAlphabetic(input);
 
+  //return true; // default validation only
+}
+function validNumber(input) {
+    // taken from getPrimeNumbers
+    // if(isNaN(input) || input == "") {
+    //     return "Please enter a valid number.";
+    // }
+
+    // return true if it is NOT a non-number AND it's not an empty string 
+    //    AND there is no space in the string 
+    //this one is for yesNo. return (!isNaN(input) && input != "" and input.indexOf(" ") == -1);
+    return( ((!isNaN(input))  &&  (input != "") && (input).indexOf(" ") == -1));
+
+    //alert("first one is: " + ((!isNaN("111"))  &&  ("111" != "") && ("111").indexOf(" ") == -1));
+    //alert("first one is: " + ((!isNaN(input))  &&  (input != "") && (input).indexOf(" ") == -1));
+
+}
 // Function to uppercase the first letter
 function upperCaseFirstLetter(thisString = ""){
   if (thisString !== ""){
@@ -246,19 +279,42 @@ function properCase(stringToProperCase) {
   return stringToProperCase.join(' ');
  }
 
-//<<<<<<< HEAD
-// function calculate_age(dobstring){ 
-//   var diff_ms = Date.now() - dob.getTime();
-//   var age_dt = new Date(diff_ms); 
-
-//   return Math.abs(age_dt.getUTCFullYear() - 1970);
-// =======
 function calculate_age(dobString) { 
     let dob = new Date(dobString);
     var diff_ms = Date.now() - dob.getTime();
     var age_dt = new Date(diff_ms); 
     return Math.abs(age_dt.getUTCFullYear() - 1970);
-//>>>>>>> 62439f13a34a7693ff38f43f1770c0bfdbcb7774
 }
 
-// console.log(calculate_age(new Date("el.age")));
+// This function was borrowed from our Problem Solving Problems code - for palindromes
+// function to strip non-alhpabetical characters
+function stripNonAlphabeticChars(stringToStrip = "") {
+  // loop through string and strip non-alhpabetical characters
+  let i = 0;
+  let stringStripped = "";
+  while(i < stringToStrip.length) {
+    if ((stringToStrip.charAt(i) >= "a" && stringToStrip.charAt(i) <= "z") 
+      || (stringToStrip.charAt(i) >= "A" && stringToStrip.charAt(i) <= "Z")) {
+      stringStripped = stringStripped + stringToStrip.charAt(i); 
+    }
+    i++;
+  }
+  return stringStripped;
+}
+
+// function to determine if string is strictly alhpabetical characters
+function isAlphabetic(inputString = "") {
+  // loop through string and strip non-alhpabetical characters
+  let i = 0;
+  let charIsAlpha = true;
+  
+  for (i=0; i < inputString.length; i++) {
+    if ( !(inputString.charAt(i) >= "a" && inputString.charAt(i) <= "z") 
+      &&  !(inputString.charAt(i) >= "A" && inputString.charAt(i) <= "Z")) {
+      return false; 
+    }
+  }
+  return true;
+}
+
+
